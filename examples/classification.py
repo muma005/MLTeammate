a53244bc96280da05ad0dc7a7f501809919e29de
G muma <2307556@students.kcau.ac.ke>
2025-06-24 11:35:57 +0300

    Restructure project and update code

--- examples/classification.py
+++ examples/classification.py
@@ -1,13 +1,13 @@
 
+# examples/classification.py
+
 from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
 from ml_teammate.interface.api import MLTeammate
 
 X, y = load_iris(return_X_y=True)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-mlt = MLTeammate()
-mlt.fit(X_train, y_train)
-preds = mlt.predict(X_test)
+automl = MLTeammate(task="classification", n_trials=5)
+automl.fit(X, y)
 
+preds = automl.predict(X)
 print("Predictions:", preds)
--- ml_teammate/search/optuna_search.py
+++ ml_teammate/search/optuna_search.py
@@ -1,4 +1,23 @@
 
-def suggest(X, y):
-    print("Suggesting config...")
-    return {"max_depth": 3, "n_estimators": 10}
+
+
+import optuna
+
+class OptunaSearch:
+    def __init__(self, config_space):
+        self.config_space = config_space
+        self.study = optuna.create_study(direction="minimize")
+        self.trials = {}
+
+    def suggest(self, trial_id, learner_name):
+        trial = self.study.ask()
+        self.trials[trial_id] = trial
+        config = self.config_space.sample(trial, learner_name)
+        return config
+
+    def report(self, trial_id, score):
+        trial = self.trials[trial_id]
+        self.study.tell(trial, score)
+
+    def get_best_config(self):
+        return self.study.best_trial.params
